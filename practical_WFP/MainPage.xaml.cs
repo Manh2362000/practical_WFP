036faf0a44c2d93998a5022e68565aea5d602a76
Manh2362000 <70956659+Manh2362000@users.noreply.github.com>
2021-10-06 10:32:20 +0700

    commit
    abc

--- practical_WFP/MainPage.xaml.cs
+++ practical_WFP/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using practical_WFP.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,11 +23,45 @@ namespace practical_WFP
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
+
     public sealed partial class MainPage : Page
     {
+        ObservableCollection<String> imagePaths = new ObservableCollection<String>();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ImagesCombo_Loaded(object sender, RoutedEventArgs e)
+        {
+            string folderPath = "Assets";
+            IEnumerable<string> filePaths = await ImageService.GetAssetFiles(folderPath);
+            foreach (var filePath in filePaths)
+            {
+                imagePaths.Add(filePath);
+            }
+        }
+
+        private void ProductList_Loaded(object sender, RoutedEventArgs e)
+        {            
+            ProductList.Items.Add(new Product
+            {
+                Name = "Sample 1",
+                Description = "Lorem ipsum",
+                ImagePath = "Assets/LockScreenLogo.scale-200.png"
+            });            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(ImagesCombo.SelectedValue);
+            ProductList.Items.Add(new Product
+            {
+                Name = NameText.Text,
+                Description = DescriptionText.Text,
+                ImagePath = (string) ImagesCombo.SelectedValue
+            });
         }
     }
 }
